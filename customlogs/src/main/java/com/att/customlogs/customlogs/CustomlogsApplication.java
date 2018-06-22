4f7ceb8c98fa8e271b865b0d56c42ed03ab1798e
Karuna <revurikarna@gmail.com>
2018-06-22 23:59:32 +0530

    Initial Custom logs file

---
+++ customlogs/src/main/java/com/att/customlogs/customlogs/CustomlogsApplication.java
@@ -0,0 +1,12 @@
+package com.att.customlogs.customlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomlogsApplication.class, args);
+	}
+}
